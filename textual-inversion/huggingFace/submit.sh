3a4b83af558f8353685458d217499a73853ce18c
SrLozano <100383511@alumnos.uc3m.es>
2023-03-09 16:29:01 +0100

    Working on HF's textual inversion

---
+++ textual-inversion/huggingFace/submit.sh
@@ -0,0 +1,52 @@
+#!/bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q gpua100
+### -- set the job Name -- 
+#BSUB -J textual-inversion
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 4 
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=4GB]"
+### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
+#BSUB -M 10GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00 
+### -- set the email address -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u s226536@dtu.dk
+### -- send notification at completion -- 
+#BSUB -N 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o Output_%J.out 
+#BSUB -e Output_%J.err 
+
+# here follow the commands you want to execute with input.in as the input file
+
+nvidia-smi
+
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export DATA_DIR="/zhome/d1/6/191852/hugging-face/diffusers/examples/textual_inversion/images"
+
+accelerate launch textual_inversion.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --train_data_dir=$DATA_DIR \
+  --learnable_property="object" \
+  --placeholder_token="<cat-toy>" --initializer_token="toy" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --max_train_steps=3000 \
+  --learning_rate=5.0e-04 --scale_lr \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --output_dir="textual_inversion_cat"
+
+
+python inference.py

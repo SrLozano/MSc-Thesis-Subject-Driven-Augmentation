0027d9ef86c226127fd4ddff8d4d6214b7f113e8
SrLozano <100383511@alumnos.uc3m.es>
2023-06-15 14:51:30 +0200

    Added README.md

--- inference.py
+++ inference.py
@@ -5,9 +5,10 @@ from visualization import plot_images, save_images
 
 prompts = ["A mysterious golden retriever approaches the great pyramids of egypt",
            "A cute photo of a golden retriever, golden colour, high quality, highly detailed, elegant, sharp focus",
-           "A realistic picture of a golden retriever on the moon, 4k, detailed, vivid colors"
+           "A realistic picture of a golden retriever on the moon, 4k, detailed, vivid colors",
+           "A cute photo of a golden retriever, high quality, highly detailed, elegant, sharp focus"
           ]
-keys = ["pyramids", "golden", "moon"]
+keys = ["pyramids", "golden", "moon", "quality"]
 
 model_id = "runwayml/stable-diffusion-v1-5"  
 number_images = 3
@@ -16,6 +17,4 @@ pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float
 
 for i in range(len(prompts)):
     images = pipe(prompts[i], num_inference_steps=50, guidance_scale=7.5, num_images_per_prompt=number_images).images
-
-    plot_images(images, keys[i], "inference")
     save_images(images, keys[i], "inference")
